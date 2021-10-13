f7df575c4e9a993b5aaf00d2f9b5d586631f8404
jacobchung <chung.jac@northeastern.edu>
2021-10-12 22:21:42 -0400

    hi

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // write your javascript code here.
 // feel free to change the pre-set attributes as you see fit
 
+// margins
 let margin = {
   top: 60,
   left: 50,
@@ -20,6 +21,7 @@ let svg = d3.select('#vis')
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
 
 
+// set j = 0 for the colors to switch off
 let j = 0
 // Add a square 
 let rect = svg.append('rect')
@@ -28,24 +30,29 @@ let rect = svg.append('rect')
   .attr('width', '20%')
   .attr('height', '20%')
   .attr('fill', '#a6cee3')
-  .on('click', function () {
+  .on('click', function () { // change the circle color
     d3.select('circle')
       .transition()
-      .attr('fill', determineColor(j))
+      .attr('fill', determineColorRedBlack(j))
     j++
-  }
-  )
+  }) // border when mouse is over
   .on("mouseover", function () {
     d3.select(this)
       .attr('stroke', '#FF007F')
-      .attr('stroke-width', '20')
-  })
+      .attr('stroke-width', '15')
+  }) // get rid of border
   .on("mouseout", function () {
     d3.select(this)
       .attr("stroke", "none")
-  })
+  }) // allow dragging
+  .call(d3.drag()
+    .on("start", dragstarted)
+    .on("drag", draggedWithXY)
+    .on("end", dragended)
+  );
 
 
+// set k = 0 for the colors to switch off
 let k = 0
 // Add a circle 
 let circle = svg.append('circle')
@@ -53,41 +60,54 @@ let circle = svg.append('circle')
   .attr('cy', '250')
   .attr('r', '60')
   .attr('fill', '#b2df8a')
-  .on("mouseover", function () {
+  .on("mouseover", function () { // border when mouse is over
     d3.select(this)
       .attr('stroke', '#FFFF00')
-      .attr('stroke-width', '20')
-  })
+      .attr('stroke-width', '15')
+  }) // get rid of border
   .on("mouseout", function () {
     d3.select(this)
       .attr("stroke", "none")
-  })
+  }) // change both shapes color when double click
   .on('dblclick', function () {
     d3.select(this)
       .transition()
-      .attr('fill', determineColor2(k))
+      .attr('fill', determineColorOrangeTeal(k))
     d3.select('rect')
       .transition()
-      .attr('fill', determineColor2(k))
+      .attr('fill', determineColorOrangeTeal(k))
     k++
-  })
-  .on("drag", function() {
-    d3.select(this)
-      .transition()
-      .attr('fill', "#0096FF")
-  })
+  }) // allow dragging
+  .call(d3.drag()
+    .on("start", dragstarted)
+    .on("drag", draggedWithCxCy)
+    .on("end", dragended)
+  );
 
 
-  
+// function to start drag
+function dragstarted() {
+  d3.select(this)
+    .raise().classed("active", true);
+}
 
-        
-  
+// drag square using x and y 
+function draggedWithXY(event) {
+  d3.select(this).attr("x", event.x).attr("y", event.y);
+}
 
+// drag circle using Cx and Cy 
+function draggedWithCxCy(event) {
+  d3.select(this).attr("cx", event.x).attr("cy", event.y);
+}
 
-
+// function to end drag
+function dragended() {
+  d3.select(this).classed("active", false);
+}
 
 // changes the circle's color between red and black for the single click
-function determineColor(c) {
+function determineColorRedBlack(c) {
   if (c % 2 == 0) {
     return '#ff0000'
   } else {
@@ -96,7 +116,7 @@ function determineColor(c) {
 }
 
 // changes both shapes colors from orange to teal for the double click
-function determineColor2(c) {
+function determineColorOrangeTeal(c) {
   if (c % 2 == 0) {
     return '#FF6700'
   } else {
